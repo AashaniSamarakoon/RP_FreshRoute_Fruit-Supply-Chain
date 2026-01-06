c449122cad6ae41f0bcf28581680c5907dd73114
savinduc1 <savindu@intrinsic-tech.net>
2026-01-06 14:05:54 +0530

    merge ashani 5/ added transporter index.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // export const BACKEND_URL = "http://192.168.1.82:4000";
 // export const BACKEND_URL = "http://192.168.1.55:4000";
-export const BACKEND_URL = "http://192.168.43.45:4000";
+export const BACKEND_URL = "http://192.168.1.100:4000";
