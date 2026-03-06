c6e1c9bd78f6f36f9873ddb56bdb072a06b5beae
savinduc1 <savindu@intrinsic-tech.net>
2026-03-06 06:03:48 +0530

    ui updates

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -5,9 +5,9 @@ import "react-native-url-polyfill/auto"; // Adds URL support for older engines
 
 // Replace these with your actual Supabase Project credentials
 // Best practice: Use process.env.EXPO_PUBLIC_SUPABASE_URL if using Expo Router .env
-const SUPABASE_URL = "https://aiwndjgadyaxuyoioxxa.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpd25kamdhZHlheHV5b2lveHhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyODEyNDMsImV4cCI6MjA3Njg1NzI0M30.YTYB_SgzsD5syELoH9xR7kiwR0u_G2fL_qVjS-3keRE";
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpd25kamdhZHlheHV5b2lveHhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyODEyNDMsImV4cCI6MjA3Njg1NzI0M30.YTYB_SgzsD5syELoH9xR7kiwR0u_G2fL_qVjS-3keRE";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
